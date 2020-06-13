07c572bdef496c098b117e67f0d0b231d882cda1
SidNotSus <idsafronov@edu.hse.ru>
2020-06-13 23:00:13 +0300

    with menu

--- game.py
+++ game.py
@@ -3,11 +3,24 @@ import sys
 import random
 import time
 #
+
 pygame.init()
 win = pygame.display.set_mode((1920,1080),pygame.FULLSCREEN)
+
 #
+
 pygame.display.set_caption("Randomizer")
+
 #
+
+bg = pygame.image.load('design/bg.jpg').convert()
+bgRightPart = pygame.image.load('design/bgRightPart.png')
+bgLeftPart = pygame.image.load('design/bgLeftPart.png')
+mainFrame = pygame.image.load('design/mainFrame.png')
+loadedPicture = pygame.image.load('1.jpg')
+
+#
+
 xStripe = 270
 yStripe = 260
 yFrame = 485
@@ -16,20 +29,81 @@ heightPhoto = 100
 widthFrame = 110
 heightFrame = 110
 students_array = []
-#
+
+############################################################################################
+############################################################################################
+############################################################################################
+############################################################################################
+
 class Student():
     def __init__(self, name, id):
         self.picture = None
         self.name = name
         self.id = id
         self.picture_path = str(id)+'.jpg'
-#
-#
-bg = pygame.image.load('design/bg.jpg').convert()
-bgRightPart = pygame.image.load('design/bgRightPart.png')
-bgLeftPart = pygame.image.load('design/bgLeftPart.png')
-mainFrame = pygame.image.load('design/mainFrame.png')
-loadedPicture = pygame.image.load('1.jpg')
+
+class Menu:
+    def __init__(self, punkts = [120, 140, u'Game', (250, 250, 30), (250, 30, 250), 0]):
+        self.punkts = punkts
+    def render(self, screen, font, num_punkt):
+        for i in self.punkts:
+            if num_punkt == i[5]:
+                screen.blit(font.render(i[2], 1, i[4]), (i[0], i[1]-30))
+            else:
+                screen.blit(font.render(i[2], 1, i[3]), (i[0], i[1]-30))
+    def menu(self):
+        done = True
+        pygame.mouse.set_visible(True)
+        pygame.key.set_repeat(0,0)
+        font_menu = pygame.font.Font('fonts/Peace Sans.otf', 50)
+        punkt = 0
+        while done:
+
+            mp = pygame.mouse.get_pos()
+            for i in self.punkts:
+                if mp[0]>i[0] and mp[0]<i[0]+155 and p[1]>i[1] and mp[1]<i[1]+50:
+                    punkt = i[5]
+            self.render(bg, font_menu, punkt)
+
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT:
+                    sys.exit()
+                if e.type == pygame.KEYDOWN:
+                    if e.key == pygame.K_RETURN:
+                        if punkt == 0:
+                            done = False
+                        if punkt == 1:
+                            sys.exit()
+                    if e.key == pygame.K_ESCAPE:
+                        sys.exit()
+                    if e.key == pygame.K_UP:
+                        if punkt > 0:
+                            punkt -= 1
+                    if e.key == pygame.K_DOWN:
+                        if punkt < len(self.punkts)-1:
+                            punkt += 1
+                if e.type == pygame.MOUSEBUTTONDOWN and e.button == 1:
+                    if punkt == 0:
+                        done = False
+                    if punkt == 1:
+                        sys.exit()
+
+        #    window.blit(info_string, (0, 0))
+            win.blit(bg, (0,0))
+            pygame.display.update()
+
+####################################################################################################
+####################################################################################################
+####################################################################################################
+####################################################################################################
+
+' создаем меню '
+punkts = [(120, 140, u'Game', (250, 250, 30), (250, 30, 250), 0),
+          (130, 210, u'Quit', (250, 250, 30), (250, 30, 250), 1)]
+game = Menu(punkts)
+game.menu()
+
+
 #
 clock = pygame.time.Clock()
 #
